4dc0c19705a224818ee3253135494691186e2a36
lukasproject1 <2187633@roc-dev.com>
2023-11-30 12:30:19 +0100

    programma met inheritance
    klaar

---
+++ Program overerven.cs
@@ -0,0 +1,18 @@
+﻿namespace overErven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people= new List<Person>();
+         Person lukas= new Person();
+            lukas.Name = "lukas";
+            people.Add(lukas);
+            Employe ik = new Employe();
+            ik.Name = "luuk";
+
+            ik.Words();
+            people.Add(ik);
+        }
+    }
+}
